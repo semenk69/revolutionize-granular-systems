6ffbeb33690cb7b9d9643ee78c8150a51e6f307e
semenk69 <152292047+semenk69@users.noreply.github.com>
2023-12-20 00:32:17 +0300

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,26 @@
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+
+def main():
+
+    data = generate_random_data()
+
+
+    for item in data:
+if __name__ == "__main__":
+def generate_random_data():
+    main()
+import random        for (int item : data) {
+
+public class RandomDataGenerator {
+            data[i] = (int) (Math.random() * 100) + 1;
+            System.out.println("Random Number: " + item);
+    }
+        int[] data = new int[10];
+
+    public static void main(String[] args) {
+}
+        for (int i = 0; i < 10; i++) {
+        }
+        }
